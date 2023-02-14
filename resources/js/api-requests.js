8f9e0b5fee520d7caef2ca69ac4e97fb46846bb6
LewisTaylor999 <93335371+LewisTaylor999@users.noreply.github.com>
2023-02-14 15:41:59 +0000

    mastodon draft

---
+++ resources/js/api-requests.js
@@ -0,0 +1,22 @@
+MastodonPostStatus();
+
+function MastodonPostStatus() {
+    fetch('https://mastodon.social/api/v1/statuses', { 
+    method: 'post', 
+    headers: new Headers({
+        'Authorization': 'Bearer ' + process.env.MASTODON_TEST_ACCESS, 
+        'Content-Type': 'application/json'
+    }), 
+    body: JSON.stringify({"status": "Hello from js"})
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (myJson) {
+        console.log(myJson);
+    })
+    .catch(function (error) {
+        console.log("Error: " + error);
+    });
+}
+
